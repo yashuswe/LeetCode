949fe3ffac58172f5e8bbd8b90dd65db81239d89
Yashassvi Suhane <yashassvishipra@gmail.com>
2023-01-04 14:54:51 +0530

    Update Minimum Rounds to Complete All Tasks.java

--- POTD/Minimum Rounds to Complete All Tasks.java
+++ POTD/Minimum Rounds to Complete All Tasks.java
@@ -5,23 +5,31 @@
  
 
 class Solution {
-    Map<Integer, Integer> memo = new HashMap<>();
     public int minimumRounds(int[] tasks) {
-        Arrays.sort(tasks);
-        int res = dp(0, tasks);
-        if(res == Integer.MAX_VALUE || res < 0) return -1;
-        return res;
-    }
-    
-    public int dp(int i, int[] tasks) {
-        if(memo.containsKey(i)) return memo.get(i);
-        if(i == tasks.length) return 0;
-        else if(i > tasks.length || i == tasks.length - 1) memo.put(i, Integer.MAX_VALUE);
-        else if(tasks[i] != tasks[i+1]) memo.put(i, Integer.MAX_VALUE);
-        else if(i+2 < tasks.length && tasks[i] == tasks[i+2]){
-            memo.put(i, 1 + Math.min(dp(i + 2, tasks), dp(i + 3, tasks)));
+        Map<Integer, Integer> freq = new HashMap();
+        // Store the frequencies in the map.
+        for (int task : tasks) {
+            freq.put(task, freq.getOrDefault(task, 0) + 1);
         }
-        else memo.put(i, 1 + dp(i + 2, tasks));
-        return memo.get(i);
+
+        int minimumRounds = 0;
+        // Iterate over the task's frequencies.
+        for (int count : freq.values()) {
+            // If the frequency is 1, it's not possible to complete tasks.
+            if (count == 1) {
+                return - 1;
+            }
+
+            if (count % 3 == 0) {
+                // Group all the task in triplets.
+                minimumRounds += count / 3;
+            } else {
+                // If count % 3 = 1; (count / 3 - 1) groups of triplets and 2 pairs.
+                // If count % 3 = 2; (count / 3) groups of triplets and 1 pair.
+                minimumRounds += count / 3 + 1;
+            }
+        }
+
+        return minimumRounds;
     }
 }
